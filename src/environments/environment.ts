e9fe4736f72b7b00c051708b91bf6d421e539835
Jobi S S <jobisjobi1234@gmail.com>
2026-03-28 22:24:38 +0530

    changed server live env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://note-taking-1-qsnb.onrender.com/api', // Adjust based on NestJS default
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'https://note-taking-1-qsnb.onrender.com/api', // Adjust based on NestJS default
+  // apiUrl: 'http://localhost:5000/api',
 };
